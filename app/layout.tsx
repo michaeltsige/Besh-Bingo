f2e5ffc3a43e5e470730bca58c7c33a6c1649008
Michael <michaeltsige11@gmail.com>
2026-04-18 17:53:42 +0300

    chore: suppresed hydration warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,6 +42,7 @@ export default function RootLayout({
     <html
       lang="en"
       className={`${inter.variable} ${outfit.variable} bg-bingo-deep-purple`}
+      suppressHydrationWarning
     >
       <body className="font-sans antialiased">
         {children}
